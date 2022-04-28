e561fd991a161e1e497733d76c3a7c25d1f2ac3b
Vagner Vieira <83680758+vagnervs@users.noreply.github.com>
2022-04-27 21:41:45 -0300

    atualização

---
+++ jogo_adivinhação_2.0.py
@@ -0,0 +1,19 @@
+import random
+comp =  random.randint(0,10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10')
+print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Digite seu palpite: '))
+    palpites += 1
+    if jogador == comp:
+        acertou = True
+    else:
+        if jogador < comp:
+            print('Mais! Tente mais uma vez. ')
+        else:
+            print('Menos! Tente novamente. ')
+print('Acertou com {} tentativas. Parabéns!'.format(palpites))
+
+
